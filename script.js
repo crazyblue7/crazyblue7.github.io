9bc3ce4b63fed3089c392f84f6dad46c112a3994
Vincenzo <visimonmora@gmail.com>
2025-02-27 18:20:25 -0300

    made a kinda big change, with css and js, a bit of html

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let quaso = "quaso";
 
-console.log(quaso);
+setInterval(() => {
+    let separatorSpace = document.getElementById("separatorSpace").value;
+    console.log(separatorSpace);
+    console.log(quaso);
+    let separator1 = document.getElementById("separator1");
+    separator1.style.fontSize =  String(separatorSpace) + "px";
+    let separator2 = document.getElementById("separator2");
+    separator2.style.fontSize =  String(separatorSpace) + "px";
+}, 30);

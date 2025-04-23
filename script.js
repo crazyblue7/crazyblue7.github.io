d87145e7d2d25d4223e1d5f11116a3c1ce367c6f
Vincenzo <visimonmora@gmail.com>
2025-04-23 17:39:52 -0300

    no commit name :|

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 let quaso = "quaso";
 
-setInterval(() => {
-    let separatorSpace = document.getElementById("separatorSpace").value;
-    console.log(separatorSpace);
-    console.log(quaso);
-    let separator1 = document.getElementById("separator1");
-    separator1.style.fontSize =  String(separatorSpace) + "px";
-    let separator2 = document.getElementById("separator2");
-    separator2.style.fontSize =  String(separatorSpace) + "px";
-}, 30);
+let bodyportfolio = document.getElementById("portfolio");
+let body = document.getElementById("mainbody");
+if (body != null) {
+    setInterval(() => {
+        let separatorSpace = document.getElementById("separatorSpace").value;
+        console.log(separatorSpace);
+        // console.log(quaso);
+        let separator1 = document.getElementById("separator1");
+        separator1.style.fontSize =  String(separatorSpace) + "px";
+        let separator2 = document.getElementById("separator2");
+        separator2.style.fontSize =  String(separatorSpace) + "px";
+    }, 30);
+}
+if (bodyportfolio != null) {
+    function doc(abajab) {
+        return document.getElementById(abajab);
+    }
+    let finishedEle = doc("finished");
+    let good = doc("goodMorning")
+    
+    let finished = false;
+    setInterval(() => {
+        finishedEle.hidden = finished;
+    }, 30);    
+}

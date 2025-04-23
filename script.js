54c151d2e781d12461ffef6f34ba6a30066a2302
Vincenzo <visimonmora@gmail.com>
2025-04-23 17:45:02 -0300

    a

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 let quaso = "quaso";
 
-let bodyportfolio = document.getElementById("portfolio");
-let body = document.getElementById("mainbody");
+function doc(abajab) {
+    return doc(abajab);
+}
+let bodyportfolio = doc("portfolio");
+let body = doc("mainbody");
 if (body != null) {
     setInterval(() => {
-        let separatorSpace = document.getElementById("separatorSpace").value;
+        let separatorSpace = doc("separatorSpace").value;
         console.log(separatorSpace);
         // console.log(quaso);
-        let separator1 = document.getElementById("separator1");
+        let separator1 = doc("separator1");
         separator1.style.fontSize =  String(separatorSpace) + "px";
-        let separator2 = document.getElementById("separator2");
+        let separator2 = doc("separator2");
         separator2.style.fontSize =  String(separatorSpace) + "px";
     }, 30);
 }
 if (bodyportfolio != null) {
-    function doc(abajab) {
-        return document.getElementById(abajab);
-    }
+    
     let finishedEle = doc("finished");
     let good = doc("goodMorning")
     
     let finished = false;
     setInterval(() => {
         finishedEle.hidden = finished;
+        let minimumSize = 20;
+        if (Number(good.style.fontSize-"px") < minimumSize) {
+            good.style.fontSize = minimumSize + "px";
+        } 
     }, 30);    
 }

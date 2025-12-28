2beeed09d78ad64377f4b867a30f3b53ed8956fe
Mewo <visimonmora@gmail.com>
2025-12-28 17:50:09 -0300

    add password gen

--- README.md
+++ README.md
@@ -3,16 +3,8 @@ this is just a stupid readme<br>
 <br>
 <br>
 <br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
 # oh you found me :|
-this repository has all my websites, THAT I CHOOSE TO PUT HERE<br>
-i made a simple login system, but its not here yet because ITS A LOGIN SYSTEM I DONT WANT ANYONES LOGINS TO BE PUBLICLY AVALIABLE<br>
+this repository has any websites i decide to make<br>
 <br>
 <br>
 <br>
---
+++ passwordgen/passwordgen.js
@@ -0,0 +1,21 @@
+let password = "";
+let min = 8;
+let max = 20;
+let len = Math.floor((Math.random()*12)+8);
+let labelius = document.getElementById("labelius");
+function main() {
+    const lowerchars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    const upperchars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    const special   =  ["1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","/","*","-","+","?"];
+    for (let i = 0; i < len +1; i++) {
+      arrayToUse = Math.floor(Math.random()*3) + 1;
+      if (arrayToUse == 1) {
+        password += lowerchars[Math.floor(Math.random()*lowerchars.length + 1)];
+      } else if (arrayToUse == 2) {
+        password += upperchars[Math.floor(Math.random()*upperchars.length + 1)];
+      } else {
+        password += special[Math.floor(Math.random()*special.length)];
+      }
+    }
+    labelius.innerHTML = "password: " + password;
+}
